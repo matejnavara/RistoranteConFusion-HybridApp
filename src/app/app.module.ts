1104ed503eb03af6e005dc928ffd2625d6752ba6
Matej Navara <Matej.navara@gmail.com>
2018-03-18 17:49:50 +0000

    Added email composer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicStorageModule } from '@ionic/Storage';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LocalNotifications } from '@ionic-native/local-notifications';
+import { EmailComposer } from '@ionic-native/email-composer';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -64,6 +65,7 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     LocalNotifications,
+    EmailComposer,
     DishProvider,
     LeaderProvider,
     PromotionProvider,
